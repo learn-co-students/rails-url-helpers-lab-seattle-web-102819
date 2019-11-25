86f0881328918094127fa1d1f077dc5ab45be7a5
bahayg <bahayg@gmail.com>
2019-11-25 11:51:46 -0800

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,12 +5,21 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
-  def show
-  end
+  # def show
+  #   @student = Student.find(params[:id])
+  # end
 
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
+  end
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
-  end
+end
+
+
